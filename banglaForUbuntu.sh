2a30924d7f0f13200cb2838e5ce30fed2b88ccf2
Towfiqul Islam <towfiq.106@gmail.com>
2020-01-01 14:08:40 +0600

    BanglaForUbuntu Shell Script

---
+++ banglaForUbuntu.sh
@@ -0,0 +1,10 @@
+wget https://github.com/maateen/avro/releases/download/v2.1/avro_2.1-3_all.deb
+
+sudo dpkg -i avro_2*.deb
+
+sudo apt-get install -f
+
+ibus restart
+
+
+#Settings > Region and language > Add an input source > Bangla > Bengali (Avro Phonetic)
