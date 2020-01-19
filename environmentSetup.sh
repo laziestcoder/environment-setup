912654bd36cffac14566138e02761c2bb4b92968
Towfiqul Islam <tawfiq.inflack@bkash.com>
2020-01-19 13:38:51 +0600

    gitignore updated and environment.sh updated

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+read -p "This script downloads and installs various softwares. Please check the script file before starting.
+Do you want to continue? (Y/N): " yn
+    case $yn in
+        [Yy]* ) echo "Thank you for continuting.";;
+        [Nn]* ) echo "Thank you. Installation aborted."; exit;;
+    esac
+
 sudo apt install snapd
 
 #open JDK
@@ -9,9 +16,9 @@ sudo apt install snapd
 sudo apt install vim && \
 
 #Tweak Tool
-#sudo apt install unity
-#sudo apt-get install unity-tweak-tool gnome-tweak-tool
-#sudo apt install dconf-tools
+sudo apt install unity
+sudo apt-get install unity-tweak-tool gnome-tweak-tool
+sudo apt install dconf-tools
 #gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 #gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 #gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
@@ -35,9 +42,9 @@ sudo snap install python38  && \
 sudo apt install git && \
 
 #Dukto
-sudo apt install gdebi -y && \
+sudo apt install gdebi  && \
 wget download.opensuse.org/repositories/home:/colomboem/xUbuntu_12.04/amd64/dukto_6.0-1_amd64.deb && \
-sudo gdebi dukto_6.0-1_amd64.deb -y  && \
+sudo gdebi dukto_6.0-1_amd64.deb  && \
 
 #Maven
 sudo apt install maven -y
@@ -67,11 +74,17 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
 
 #Oracle JDK
-cd ~/Downloads && \
-sudo mkdir -p /var/cache/oracle-jdk11-installer-local/ && \
-sudo cp jdk-11.*.tar.gz /var/cache/oracle-jdk11-installer-local/ && \
-sudo add-apt-repository ppa:linuxuprising/java && \
-sudo apt install oracle-java11-set-default-local && \
+while true; do
+    read -p "Do you have the oracle jdk.11.*.tar.gz in your 'Downloads' folder? (Y/N): " yn
+    case $yn in
+        [Yy]* ) cd ~/Downloads && \
+          sudo mkdir -p /var/cache/oracle-jdk11-installer-local/ && \
+          sudo cp jdk-11.*.tar.gz /var/cache/oracle-jdk11-installer-local/ && \
+          sudo add-apt-repository ppa:linuxuprising/java && \
+          sudo apt install oracle-java11-set-default-local; break;;
+        [Nn]* ) echo "PLease download the oracle jdk.11.*.tar.gz and run the script angain. Thank you."; break;;
+    esac
+done
 
 #Postman
 sudo snap install postman
@@ -81,7 +94,8 @@ sudo apt-get install docker-ce -y && \
 apt-cache madison docker-ce && \
 
 #Docker-Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" \
+      -o /usr/local/bin/docker-compose && \
 sudo chmod +x /usr/local/bin/docker-compose && \
 sudo ln -fs /usr/local/bin/docker-compose /usr/bin/docker-compose && \
 
