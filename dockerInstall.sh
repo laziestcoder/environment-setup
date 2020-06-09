6752687504a6be96a2a0f3e8d10d47489ac7a9d3
towfiq <tawfiq.inflack@bkash.com>
2020-06-09 18:30:21 +0600

    file updated

--- dockerInstall.sh
+++ dockerInstall.sh
@@ -16,3 +16,20 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl unmask docker
 sudo systemctl start docker
+
+#Docker
+#read -p "Do you want to install Docker? (Y/N): " yn
+#case $yn in
+#[Yy]*) sudo apt-get install docker-ce -y && apt-cache madison docker-ce ;;
+#[Nn]*) echo "Thank you. Docker Installation aborted." ;;
+#esac
+
+#Docker-Compose
+#read -p "Do you want to install Docker-Compose? (Y/N): " yn
+#case $yn in
+#[Yy]*) sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" \
+#  -o /usr/local/bin/docker-compose &&
+#  sudo chmod +x /usr/local/bin/docker-compose &&
+#  sudo ln -fs /usr/local/bin/docker-compose /usr/bin/docker-compose ;;
+#[Nn]*) echo "Thank you. Docker-Compose Installation aborted." ;;
+#esac
--- environmentSetup.sh
+++ environmentSetup.sh
@@ -134,23 +134,6 @@ case $yn in
 [Nn]*) echo "Thank you. Postman Installation aborted." ;;
 esac
 
-#Docker
-read -p "Do you want to install Docker? (Y/N): " yn
-case $yn in
-[Yy]*) sudo apt-get install docker-ce -y && apt-cache madison docker-ce ;;
-[Nn]*) echo "Thank you. Docker Installation aborted." ;;
-esac
-
-#Docker-Compose
-read -p "Do you want to install Docker-Compose? (Y/N): " yn
-case $yn in
-[Yy]*) sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" \
-  -o /usr/local/bin/docker-compose &&
-  sudo chmod +x /usr/local/bin/docker-compose &&
-  sudo ln -fs /usr/local/bin/docker-compose /usr/bin/docker-compose ;;
-[Nn]*) echo "Thank you. Docker-Compose Installation aborted." ;;
-esac
-
 #ngrok
 #write installation commands for ngrok
 
