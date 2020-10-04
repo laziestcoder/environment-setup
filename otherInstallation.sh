64192c8165f51e762b6532f9bc68e3416eeffefd
towfiq <tawfiq.inflack@bkash.com>
2020-10-04 21:27:02 +0600

    Update on oct 4, 2020

--- otherInstallation.sh
+++ otherInstallation.sh
@@ -3,3 +3,5 @@ wget https://zoom.us/client/latest/zoom_amd64.deb
 sudo apt install ./zoom_amd64.deb
 echo "Installing XCLIP ... "
 sudo apt install xclip
+suod apt install gimp
+sudo snap install vlc
