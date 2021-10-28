d91ab14268bf7178cf0f19c853a222b8d7585d20
towfiq <towfiq.106@gmail.com>
2021-10-29 01:13:59 +0600

    compiler install file renamed

--- otherInstallation.sh
+++
@@ -1,7 +0,0 @@
-echo "Zoom Installation Started ...";
-wget https://zoom.us/client/latest/zoom_amd64.deb
-sudo apt install ./zoom_amd64.deb
-echo "Installing XCLIP ... "
-sudo apt install xclip
-suod apt install gimp
-sudo snap install vlc
