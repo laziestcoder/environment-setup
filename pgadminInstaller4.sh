0a21fa837177d2a1ab0b48dda16bebfea12dd0dc
towfiq <towfiq.106@gmail.com>
2022-07-27 20:19:37 +0600

    pgadmin4 installer

---
+++ pgadminInstaller4.sh
@@ -0,0 +1,5 @@
+curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add -
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/focal pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
+
+sudo apt update
+sudo apt install pgadmin4
