46c5bfdb30cad7dd0ad22ee3573cac99dd212f83
towfiq <towfiq.106@gmail.com>
2024-06-04 02:35:50 +0600

    updated pgadmin

--- pgadminInstaller4.sh
+++ pgadminInstaller4.sh
@@ -1,5 +1,6 @@
-curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add -
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add -
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/focal pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
-
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
 sudo apt update
 sudo apt install pgadmin4
